08b8f2c3b53fb3c2c81d06d685085bffc7795eef
Gleb Ivanov <s22223@pjwstk.edu.pl>
2023-07-17 23:32:15 +0200

    back end init

---
+++ AuthorizeApp/AuthorizeApp/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetLogin()
+        {
+            string answer = "Hello there";
+            return Ok($"{answer}");
+        }
+    }
+}
